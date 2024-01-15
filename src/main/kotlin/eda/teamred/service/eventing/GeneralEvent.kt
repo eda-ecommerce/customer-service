a90fdf2eeccee5f38927d8ec962be32e7438e8bf
JannikAlx <jannik-alx@gmx.de>
2024-01-15 18:09:52 +0100

    chore: rename type to operation

--- src/main/kotlin/eda/teamred/service/eventing/GeneralEvent.kt
+++ src/main/kotlin/eda/teamred/service/eventing/GeneralEvent.kt
@@ -1,13 +1,10 @@
 package eda.teamred.service.eventing
 
 import com.google.gson.Gson
-import com.google.gson.JsonElement
 import eda.teamred.service.model.DTO
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.header.internals.RecordHeaders
-import java.time.LocalDateTime
 
-open class GeneralEvent (val type: Operation, val payload: DTO) {
+open class GeneralEvent (val operation: Operation, val payload: DTO) {
     val source: String = "customer-service"
     val timestamp = System.currentTimeMillis() / 1000
 
@@ -15,7 +12,7 @@ open class GeneralEvent (val type: Operation, val payload: DTO) {
         val gson = Gson()
         val payloadJson = gson.toJson(payload)
         val record = ProducerRecord<String, String>(topic, payloadJson).apply {
-            headers().add("operation", type.toString().toByteArray())
+            headers().add("operation", operation.toString().toByteArray())
             headers().add("source", source.toByteArray())
             headers().add("timestamp", timestamp.toString().toByteArray())
         }
