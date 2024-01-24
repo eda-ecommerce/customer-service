74e7f95a73815a379808c12d2c43c80fcaf06c75
JannikAlx <jannik-alx@gmx.de>
2024-01-24 01:59:24 +0100

    fix: allow phone number and email to be null

--- src/main/kotlin/eda/teamred/service/model/Customer.kt
+++ src/main/kotlin/eda/teamred/service/model/Customer.kt
@@ -11,14 +11,13 @@ class Customer(
     var lastName: String,
     @Embedded
     var address: Address,
-    var email : String,
-    var phoneNumber : String,
+    var email : String?,
+    var phoneNumber : String?,
     @Id
     val id: UUID
 
 ){
-    constructor(firstName: String,lastName: String,address: Address) : this(firstName,lastName,address,"","",UUID.randomUUID())
-    constructor(firstName: String,lastName: String,address: Address,email: String,phoneNumber: String) : this(firstName,lastName,address,email,phoneNumber,UUID.randomUUID())
-    constructor(firstName: String,lastName: String,address: Address,id: UUID) : this(firstName,lastName,address,"","",id)
+    constructor(firstName: String,lastName: String,address: Address) : this(firstName,lastName,address,null,null,UUID.randomUUID())
+    constructor(firstName: String,lastName: String,address: Address,email: String?,phoneNumber: String?) : this(firstName,lastName,address,email,phoneNumber,UUID.randomUUID())
 
 }
